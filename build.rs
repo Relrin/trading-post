a295abe9bd4aa16cf91048361720caae29ae5d63
Relrin <relrin78@gmail.com>
2023-09-16 17:22:14 +0200

    Added protobuf for the auction

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::fs::create_dir_all;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let current_dir = PathBuf::new();
+
+    let proto_dir = &[current_dir.join("proto")];
+    let out_dir = current_dir.join("src").join("proto");
+    create_dir_all(out_dir.clone())?;
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .out_dir(out_dir)
+        .compile(&[current_dir.join("proto/auction.proto")], proto_dir)?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,39 @@
-mod api;
+//mod api;
 mod cli;
-mod core;
-mod models;
+//mod core;
+//mod models;
 
-use actix_web::{middleware, web, App, HttpServer};
+//use actix_web::{middleware, web, App, HttpServer};
 use structopt::StructOpt;
 
-use crate::api::auction::api::get_auction_router;
-use crate::api::k8s::get_k8s_router;
+//use crate::api::auction::api::get_auction_router;
+//use crate::api::k8s::get_k8s_router;
 use crate::cli::CliOptions;
-use crate::core::error::transform_actix_web_validator_error;
-use crate::core::orm::session::create_cassandra_session;
+//use crate::core::error::transform_actix_web_validator_error;
+//use crate::core::orm::session::create_cassandra_session;
 
-const MAX_JSON_SIZE: usize = 4096;
-
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     let opts = CliOptions::from_args();
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    let cassandra_session = create_cassandra_session(&opts).await;
+    // let cassandra_session = create_cassandra_session(&opts).await;
+    //
+    // HttpServer::new(move || {
+    //     App::new()
+    //         .wrap(middleware::Logger::default().log_target("http_log"))
+    //         .app_data(web::JsonConfig::default().limit(MAX_JSON_SIZE))
+    //         .app_data(web::Data::new(cassandra_session.clone()))
+    //         .app_data(
+    //             actix_web_validator::QueryConfig::default()
+    //                 .error_handler(|err, req| transform_actix_web_validator_error(err, req)),
+    //         )
+    //         .service(get_auction_router())
+    //         .service(get_k8s_router())
+    // })
+    // .bind((opts.host, opts.port))?
+    // .run()
+    // .await
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default().log_target("http_log"))
-            .app_data(web::JsonConfig::default().limit(MAX_JSON_SIZE))
-            .app_data(web::Data::new(cassandra_session.clone()))
-            .app_data(
-                actix_web_validator::QueryConfig::default()
-                    .error_handler(|err, req| transform_actix_web_validator_error(err, req)),
-            )
-            .service(get_auction_router())
-            .service(get_k8s_router())
-    })
-    .bind((opts.host, opts.port))?
-    .run()
-    .await
+    Ok(())
 }
