320f840f5582f12001201adbc604a77123dde15e
Relrin <relrin78@gmail.com>
2023-09-23 01:25:09 +0200

    Added convert to Trade from creation request

--- src/models/trade.rs
+++ src/models/trade.rs
@@ -1,13 +1,11 @@
-use crate::core::error::Error;
-use crate::core::error::Error::ValidationError;
-
 use cdrs_tokio::query::QueryValues;
 use cdrs_tokio::query_values;
 use cdrs_tokio_helpers_derive::{IntoCdrsValue, TryFromRow};
-use chrono::{DateTime, Days, Duration, Utc};
-use futures::future::err;
+use chrono::{DateTime, Days, Utc};
 use lazy_static::lazy_static;
 use serde::{Deserializer, Serialize};
+use std::str::FromStr;
+use std::time::Duration;
 use uuid::Uuid;
 
 use crate::proto::CreateTradeRequest;
@@ -48,33 +46,10 @@ pub struct Trade {
     is_deleted: bool,
 }
 
-// #[derive(Debug, PartialEq)]
-// pub struct CreateTrade {
-//     item_id: Uuid,
-//     item_name: String, //  #[validate(length(min = 1))]
-//     bid_price: i64,    //  #[validate(range(min = 0))]
-//     buyout_price: i64,
-//     created_by: Uuid,
-//     created_by_username: String, // #[validate(length(min = 1))]
-//     expire_in: i64,
-// }
-
 #[derive(Debug)]
 pub struct TradeOperation {
     price: i64, // #[validate(range(min = 1))]
 }
-
-// fn validate_create_trade(instance: &CreateTrade) -> Result<(), ValidationError> {
-//     if let Some(buyout_price) = instance.buyout_price {
-//         if buyout_price > 0 && instance.bid_price > buyout_price {
-//             return Err(ValidationError::new(
-//                 "The bid can't be greater than the buyout price",
-//             ));
-//         }
-//     }
-//
-//     Ok(())
-// }
 
 impl Trade {
     pub fn item_id(&self) -> Uuid {
@@ -115,33 +90,28 @@ impl Trade {
     }
 }
 
-// impl From<CreateTradeRequest> for Trade {
-//     fn from(instance: CreateTradeRequest) -> Self {
-//         let buyout_price = match instance.buyout_price {
-//             Some(value) => value,
-//             None => 0,
-//         };
-//
-//         let created_at = Utc::now();
-//         let expired_at = match instance.expire_in {
-//             Some(duration) => created_at + duration,
-//             // Set a date before the created_at date to indicate that expiration wasn't set
-//             None => created_at - Days::new(1),
-//         };
-//
-//         Self {
-//             id: Uuid::new_v4(),
-//             item_id: instance.item_id,
-//             item_name: instance.item_name,
-//             bid_price: instance.bid_price,
-//             buyout_price,
-//             created_by: instance.created_by,
-//             created_by_username: instance.created_by_username.clone(),
-//             created_at,
-//             bought_by: *EMPTY_UUID,
-//             bought_by_username: String::new(),
-//             expired_at,
-//             is_deleted: false,
-//         }
-//     }
-// }
+impl From<CreateTradeRequest> for Trade {
+    fn from(request: CreateTradeRequest) -> Self {
+        let created_at = Utc::now();
+        let expired_at = match request.expire_in {
+            // Set a date before the created_at date to indicate that expiration wasn't set
+            0 => created_at - Days::new(1),
+            _ => created_at + Duration::from_secs(request.expire_in as u64),
+        };
+
+        Self {
+            id: Uuid::new_v4(),
+            item_id: Uuid::from_str(&request.item_id).unwrap(),
+            item_name: request.item_name,
+            bid_price: request.bid_price,
+            buyout_price: request.buyout_price,
+            created_by: Uuid::from_str(&request.created_by).unwrap(),
+            created_by_username: request.created_by_username,
+            created_at,
+            bought_by: *EMPTY_UUID,
+            bought_by_username: String::new(),
+            expired_at,
+            is_deleted: false,
+        }
+    }
+}
